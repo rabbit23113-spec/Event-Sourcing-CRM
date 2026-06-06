3f56cccf0f9885fb6c88673d8fe717b595c05a88
Raffinnaisse <rabbit23113@gmail.com>
2026-06-06 12:05:21 +0300

    Events microservice: Dockerfile + main.ts

--- event-sourcing-crm/apps/events-microservice/src/main.ts
+++ event-sourcing-crm/apps/events-microservice/src/main.ts
@@ -3,18 +3,25 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import {Logger} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app/app.module';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@localhost:5672`],
+      queue: '???-queue',
+      queueOptions: {
+        durable: true,
+      },
+    }
+  });
+  await app.listen();
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `🚀 Application is listening on RabbitMQ`,
   );
 }
 
