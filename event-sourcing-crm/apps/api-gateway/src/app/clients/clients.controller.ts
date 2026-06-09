bd68cda409013e997cbd33f9bf1bf7784b750a55
Raffinnaisse <rabbit23113@gmail.com>
2026-06-09 11:02:59 +0300

    clients microservice: service and controller in api gateway

--- event-sourcing-crm/apps/api-gateway/src/app/clients/clients.controller.ts
+++ event-sourcing-crm/apps/api-gateway/src/app/clients/clients.controller.ts
@@ -1,7 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 import { ClientsService } from './clients.service';
+import {ApiOperation, ApiParam, ApiResponse} from "@nestjs/swagger";
+import {ClientDto} from "../dto/clients/client.dto";
+import {CreateClientDto} from "../dto/clients/create-client.dto";
+import {UpdateClientDto} from "../dto/clients/update-client.dto";
 
 @Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
+
+  @ApiOperation({summary: "Get all clients"})
+  @ApiResponse({status: 200, type: ClientDto, isArray: true})
+  @Get("find")
+  async findAll(): Promise<ClientDto[]> {
+    return await this.clientsService.findAll();
+  }
+
+  @ApiOperation({summary: "Get client by id"})
+  @ApiResponse({status: 200, type: ClientDto})
+  @ApiParam({name: 'id'})
+  @Get("find/id/:id")
+  async findOne(@Param('id') id: string): Promise<ClientDto> {
+    return await this.clientsService.findOne(id);
+  }
+
+  @ApiOperation({summary: "Get clients by company name"})
+  @ApiResponse({status: 200, type: ClientDto, isArray: true})
+  @ApiParam({name: 'company'})
+  @Get("find/company/:company")
+  async findByCompanyName(@Param('company') company: string): Promise<ClientDto[]> {
+    return await this.clientsService.findByCompanyName(company)
+  }
+
+  @ApiOperation({summary: "Get client by name"})
+  @ApiResponse({status: 200, type: ClientDto})
+  @ApiParam({name: 'name'})
+  @Get("find/name/:name")
+  async findName(@Param('name') name: string): Promise<ClientDto> {
+    return await this.clientsService.findOneByName(name);
+  }
+
+  @ApiOperation({summary: "Create client" })
+  @ApiResponse({status: 201, type: ClientDto})
+  @Post("create")
+  async create(@Body() createClientDto: CreateClientDto): Promise<ClientDto> {
+    return await this.clientsService.createOne(createClientDto)
+  }
+
+  @ApiOperation({summary: "Update client" })
+  @ApiResponse({status: 204})
+  @Patch("update")
+  async updateOne(@Body() updateClientDto: UpdateClientDto): Promise<void> {
+    return await this.clientsService.updateOne(updateClientDto)
+  }
+
+  @ApiOperation({summary: "Delete client" })
+  @ApiResponse({status: 204})
+  @Delete("delete/:id")
+  async deleteOne(@Param('id') id: string): Promise<void> {
+    return await this.clientsService.deleteOne(id)
+  }
 }
--- event-sourcing-crm/apps/api-gateway/src/app/clients/clients.service.ts
+++ event-sourcing-crm/apps/api-gateway/src/app/clients/clients.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
+import {RMQ_CLIENTS_CLIENT_ID} from "../constants/constants";
+import {ClientProxy} from "@nestjs/microservices";
+import {CACHE_MANAGER} from "@nestjs/cache-manager";
+import type {Cache} from "cache-manager";
+import {firstValueFrom} from "rxjs";
+import {UpdateClientDto} from "../dto/clients/update-client.dto";
+import {CreateClientDto} from "../dto/clients/create-client.dto";
+import {ClientDto} from "../dto/clients/client.dto";
 
 @Injectable()
-export class ClientsService {}
+export class ClientsService {
+  constructor(@Inject(RMQ_CLIENTS_CLIENT_ID) private readonly client: ClientProxy, @Inject(CACHE_MANAGER) private readonly cache: Cache) {
+  }
+
+  async findAll(): Promise<ClientDto[]> {
+    const cachedClients: ClientDto[] | undefined = await this.cache.get("clients:all")
+    if (cachedClients) {
+      return cachedClients;
+    }
+    const clients = await firstValueFrom(this.client.send({cmd: "clients.microservice: findAll"}, {}))
+    await this.cache.set("clients:all", clients);
+    return clients;
+  }
+
+  async findOne(id: string): Promise<ClientDto> {
+    const cachedLead: ClientDto | undefined = await this.cache.get(`clients:${id}`);
+    if (cachedLead) {
+      return cachedLead
+    }
+    const lead = await firstValueFrom(this.client.send({cmd: "clients.microservice: findOne"}, {id}))
+    await this.cache.set(`clients:${id}`, lead);
+    return lead
+  }
+
+  async findByCompanyName(companyName: string): Promise<ClientDto[]> {
+    const cachedClients: ClientDto[] | undefined = await this.cache.get(`clients:${companyName}`);
+    if (cachedClients) {
+      return cachedClients;
+    }
+    const clients = await firstValueFrom(this.client.send({cmd: "clients.microservice: findByCompanyName"}, {companyName}))
+    await this.cache.set(`clients:${companyName}`, clients);
+    return clients
+  }
+
+  async findOneByName(name: string): Promise<ClientDto> {
+    const cachedLead: ClientDto | undefined = await this.cache.get(`clients:${name}`);
+    if (cachedLead) {
+      return cachedLead
+    }
+    const lead = await firstValueFrom(this.client.send({cmd: "clients.microservice: findOneByName"}, {name}))
+    await this.cache.set(`clients:${name}`, lead);
+    return lead
+  }
+
+  async createOne(dto: CreateClientDto): Promise<ClientDto> {
+    return await firstValueFrom(this.client.send({cmd: "clients.microservice: createOne"}, {dto}))
+  }
+
+  async updateOne(dto: UpdateClientDto): Promise<void> {
+    return await firstValueFrom(this.client.emit({cmd: "clients.microservice: updateOne"}, {dto}))
+  }
+
+  async deleteOne(id: string): Promise<void> {
+    return await firstValueFrom(this.client.emit({cmd: "clients.microservice: deleteOne"}, {id}))
+  }
+}
