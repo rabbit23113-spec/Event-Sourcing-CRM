5fd3305c85c02017645c646382f83b599796ddb4
Raffinnaisse <rabbit23113@gmail.com>
2026-06-10 11:07:22 +0300

    notes microservice: notes service in api gateway

--- event-sourcing-crm/apps/api-gateway/src/app/notes/notes.module.ts
+++ event-sourcing-crm/apps/api-gateway/src/app/notes/notes.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { NotesController } from './notes.controller';
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import * as constants from "../constants/constants";
+import {CacheModule} from "@nestjs/cache-manager";
+import {redisStore} from "cache-manager-redis-store";
 
 @Module({
+  imports: [
+    ClientsModule.register([{
+      name: constants.RMQ_NOTES_CLIENT_ID,
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://${constants.RMQ_USER}:${constants.RMQ_PASS}@${constants.RMQ_HOST}:${constants.RMQ_PORT}`],
+        queue: constants.RMQ_NOTES_QUEUE,
+        queueOptions: {
+          durable: true,
+        },
+      }
+    }]),
+    CacheModule.register({
+      store: redisStore,
+      host: constants.REDIS_HOST,
+      port: constants.REDIS_PORT,
+      ttl: 60,
+    }),
+  ],
   controllers: [NotesController],
   providers: [NotesService],
 })
--- event-sourcing-crm/apps/api-gateway/src/app/notes/notes.service.ts
+++ event-sourcing-crm/apps/api-gateway/src/app/notes/notes.service.ts
@@ -1,4 +1,88 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
+import {RMQ_NOTES_CLIENT_ID} from "../constants/constants";
+import {ClientProxy, MessagePattern} from "@nestjs/microservices";
+import {CACHE_MANAGER} from "@nestjs/cache-manager";
+import type {Cache} from "cache-manager";
+import {NoteDto} from "../dto/notes/note.dto";
+import {CreateNoteDto} from "../dto/notes/create-note.dto";
+import {UpdateNoteDto} from "../dto/notes/update-note.dto";
+import {firstValueFrom} from "rxjs";
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+  constructor(@Inject(RMQ_NOTES_CLIENT_ID) private readonly client: ClientProxy, @Inject(CACHE_MANAGER) private readonly cache: Cache) {
+  }
+
+  @MessagePattern({cmd: "notes.microservice: findAll"})
+  async findAll(): Promise<NoteDto[]> {
+    const cachedNotes: NoteDto[] | undefined = await this.cache.get("notes:all")
+    if (cachedNotes) {
+      return cachedNotes
+    }
+    const response = await firstValueFrom(this.client.send({cmd: "notes.microservice: findAll"}, {}))
+    await this.cache.set("notes:all", response)
+    return response;
+  }
+
+  async findOne(id: string): Promise<NoteDto> {
+    const cachedNote: NoteDto | undefined = await this.cache.get(`notes:${id}`);
+    if (cachedNote) {
+      return cachedNote;
+    }
+    const response = await firstValueFrom(this.client.send({cmd: "notes.microservice: findOne"}, {id}))
+    await this.cache.set(`notes:${id}`, response)
+    return response;
+  }
+
+  async findByAuthorId(authorId: string): Promise<NoteDto[]> {
+    const cachedNotes: NoteDto[] | undefined = await this.cache.get(`notes:${authorId}`);
+    if (cachedNotes) {
+      return cachedNotes;
+    }
+    const response = await firstValueFrom(this.client.send({cmd: "notes.microservice: findByAuthorId"}, {authorId}))
+    await this.cache.set(`notes:${authorId}`, response)
+    return response;
+  }
+
+  async findByClientId(clientId: string): Promise<NoteDto[]> {
+    const cachedNotes: NoteDto[] | undefined = await this.cache.get(`notes:${clientId}`);
+    if (cachedNotes) {
+      return cachedNotes;
+    }
+    const response = await firstValueFrom(this.client.send({cmd: "notes.microservice: findByClientId"}, {clientId}))
+    await this.cache.set(`notes:${clientId}`, response)
+    return response;
+  }
+
+  async findByLeadId(leadId: string): Promise<NoteDto[]> {
+    const cachedNotes: NoteDto[] | undefined = await this.cache.get(`notes:${leadId}`);
+    if (cachedNotes) {
+      return cachedNotes;
+    }
+    const response = await firstValueFrom(this.client.send({cmd: "notes.microservice: findByLeadId"}, {leadId}));
+    await this.cache.set(`notes:${leadId}`, response)
+    return response;
+  }
+
+  async findByDealId(dealId: string): Promise<NoteDto[]> {
+    const cachedNotes: NoteDto[] | undefined = await this.cache.get(`notes:${dealId}`);
+    if (cachedNotes) {
+      return cachedNotes;
+    }
+    const response = await firstValueFrom(this.client.send({cmd: "notes.microservice: findByDealId"}, {dealId}))
+    await this.cache.set(`notes:${dealId}`, response)
+    return response;
+  }
+
+  async createOne(dto: CreateNoteDto): Promise<NoteDto> {
+    return await firstValueFrom(this.client.send({cmd: "notes.microservice: createOne"}, {dto}))
+  }
+
+  async updateOne(dto: UpdateNoteDto): Promise<void> {
+    this.client.emit({cmd: "notes.microservice: updateOne"}, {dto})
+  }
+
+  async deleteOne(id: string): Promise<void> {
+    this.client.emit({cmd: "notes.microservice: deleteOne"}, {id})
+  }
+}
