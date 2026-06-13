4253cddcfd9b39663526f223589d9260a91c6d91
Raffinnaisse <rabbit23113@gmail.com>
2026-06-13 12:05:19 +0300

    refactoring: init e2e api gateway

--- event-sourcing-crm/apps/api-gateway-e2e/src/support/test-setup.ts
+++ event-sourcing-crm/apps/api-gateway-e2e/src/support/test-setup.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 import axios from 'axios';
 
-module.exports = async function() {
+module.exports = async function () {
   // Configure axios for tests to use.
-  const host = process.env.HOST ?? 'localhost';
-  const port = process.env.PORT ?? '3000';
+  const host = "localhost";
+  const port = "13050";
   axios.defaults.baseURL = `http://${host}:${port}`;
 };
