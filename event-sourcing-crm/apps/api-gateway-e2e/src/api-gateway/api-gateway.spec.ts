188b68daf6aebc5070d6c5652c77830423f8ea15
Raffinnaisse <rabbit23113@gmail.com>
2026-06-13 11:50:45 +0300

    refactoring: clean up

--- event-sourcing-crm/apps/api-gateway-e2e/src/api-gateway/api-gateway.spec.ts
+++ event-sourcing-crm/apps/api-gateway-e2e/src/api-gateway/api-gateway.spec.ts
@@ -8,3 +8,14 @@ describe('GET /api', () => {
     expect(res.data).toEqual({ message: 'Hello API' });
   });
 })
+
+describe('API Gateway', async () => {
+  describe('GET /api', () => {
+    it('should return a message', async () => {
+      const res = await axios.get(`/api`);
+
+      expect(res.status).toBe(200);
+      expect(res.data).toEqual({ message: 'Hello Event Sourcing CRM API' });
+    });
+  })
+})
